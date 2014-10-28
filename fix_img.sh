3aba778cb48e30f98fbba841cc3c2f6a4446b762
pimgeek <wudibiz@163.com>
2014-10-28 22:34:35 +0800

    微调 fix_img.sh 脚本

--- fix_img.sh
+++ fix_img.sh
@@ -3,14 +3,14 @@
 ./pub_to_github.bat
 
 echo
-find ./zim -name "*.html" -exec sed -i -r 's/<img src=".*http:\/([^"]*)"/<img src="http:\/\/\1"/ig' '{}' \; 
+find ./zim -name "*.html" -exec sed -i -r 's/src=".*\/http:\/([^"]*)"/src="http:\/\/\1"/ig' '{}' \; 
 
 echo 图片地址已经修复完毕。
-echo 按回车键继续……
-read
+sleep 2
 
 echo
 echo 下面开始提交网站内容的新版本。
+sleep 2
 git add ./zim/.
 git add ./templates/*.html
 git add ./stylesheets/*.css
