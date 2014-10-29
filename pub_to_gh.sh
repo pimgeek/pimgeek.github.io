fb415f3feb7561e1dc3e325c60f013e81c64fdaa
pimgeek <wudi.biz@gmail.com>
2014-10-29 10:26:06 +0800

    调整 zim 网站内容导出脚本的名称，并且修改相关脚本，以便反映实际的脚本功能。

--- pub_to_gh.sh
+++ pub_to_gh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./pub_to_github.bat
+./zim_refresh_site_content.bat
 
 echo
 find ./zim -name "*.html" -exec sed -i -r 's/src=".*\/http:\/([^"]*)"/src="http:\/\/\1"/ig' '{}' \; 
@@ -8,7 +8,6 @@ find ./zim -name "*.html" -exec sed -i -r 's/src=".*\/http:\/([^"]*)"/src="http:
 echo 图片地址已经修复完毕。
 sleep 2
 
-echo
 echo 下面开始提交网站内容的新版本。
 sleep 2
 git add ./zim/.
