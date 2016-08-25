b6cc143c8533de523a9c09595cd38665cb30299e
pimgeek <wudibiz@163.com>
2016-08-26 03:58:17 +0800

    使用 dagre-d3 作为导航图渲染工具，大大减少首次载入时间。

---
+++ leo/dagre_utils.js
@@ -0,0 +1,45 @@
+//@+leo-ver=5-thin
+//@+node:pimgeek.20160826003911.1: * @file dagre_utils.js
+// Input related code goes here
+
+var inputGraph = document.querySelector("#dot_code");
+
+// Set up zoom support
+var svg = d3.select("svg"),
+  inner = d3.select("svg g");
+  zoom = d3.behavior.zoom().on("zoom", function() {
+    inner.attr("transform", "translate(" + d3.event.translate + ")" +
+      "scale(" + d3.event.scale + ")");
+  });
+
+svg.call(zoom);
+
+// Create and configure the renderer
+var render = dagreD3.render();
+
+function tryDraw() {
+  var g;
+  inputGraph.setAttribute("class", "");
+  try {
+    g = graphlibDot.read(inputGraph.innerHTML);
+  } catch (e) {
+    inputGraph.setAttribute("class", "error");
+    throw e;
+  }
+
+  // Set margins, if not present
+  if (!g.graph().hasOwnProperty("marginx") &&
+    !g.graph().hasOwnProperty("marginy")) {
+    g.graph().marginx = 20;
+    g.graph().marginy = 20;
+  }
+
+  g.graph().transition = function(selection) {
+    return selection.transition().duration(500);
+  };
+
+  // Render the graph into svg g
+  d3.select("svg g")
+    .call(render, g);
+}
+//@-leo
